0088149cc825b1704abd491ea928940fc991572a
Miguel Niblock <miguelniblock@posteo.net>
2020-09-12 23:47:07 -0700

    implemented createContext automation

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 // named imports are necessary for non-default, named exports
-import {BlogProvider} from './src/context/BlogContext';
+import {Provider} from './src/context/BlogContext';
 import IndexScreen from './src/screens/IndexScreen';
 
 const navigator = createStackNavigator(
@@ -21,6 +21,6 @@ const navigator = createStackNavigator(
 const App = createAppContainer(navigator);
 
 export default () => {
-  return <BlogProvider><App /></BlogProvider>
+  return <Provider><App /></Provider>
 };
 // App will be the children of BlogContext
