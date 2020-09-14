c69549ca4fb66d852faf506a36c6a9ac285e5f3d
Miguel Niblock <miguelniblock@posteo.net>
2020-09-14 12:20:28 -0700

    edit screen can edit with textinput

--- src/screens/EditScreen.js
+++ src/screens/EditScreen.js
@@ -1,9 +1,20 @@
-import React from 'react';
-import {Text, View, StyleSheet} from 'react-native';
+import React,{useContext, useState} from 'react';
+import {Text, View, StyleSheet, TextInput} from 'react-native';
+import {Context} from '../context/BlogContext';
 
 const EditScreen = ({navigation})=> {
+
+  const {state} = useContext(Context);
+  const post = state.find((post)=>post.id === navigation.getParam('id'));
+
+  const [title, setTitle] = useState(post.title);
+  const [content, setContent] = useState(post.content);
+
   return (
-    <Text>EditScreen - {navigation.getParam('id')}</Text>
+    <View>
+      <Text>EditScreen - {navigation.getParam('id')}</Text>
+      <TextInput value={title} onChangeText={(newTitle)=>setTitle(newTitle)} />
+      </View>
   )
 };
 
